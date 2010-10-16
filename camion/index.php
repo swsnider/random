91ec0790c1138e3d8873edb9ea6266f257f69500
Silas Snider <swsnider@gmail.com>
2010-10-16 14:40:49 -0400

    testing

--- camion/index.php
+++ camion/index.php
@@ -1,7 +1,6 @@
 <?php
 	
 ?>
-
 <html>
   <head>
     <title>Camion</title>
@@ -13,13 +12,31 @@
           upload_url: "upload.php",
           flash_url: "swfupload.swf",
           flash9_url: "swfupload_fp9.swf",
-          file_size_limit: "1000 MB"
+          file_size_limit: "1000 MB",
+  				button_width: "65",
+  				button_height: "29",
+  				button_placeholder_id: "spanButtonPlaceHolder",
+  				button_text: '<span class="theFont">Hello</span>',
+  				button_text_style: ".theFont { font-size: 16; }",
+  				button_text_left_padding: 12,
+  				button_text_top_padding: 3,
         });
-        
       }
     </script>
   </head>
   <body>
-    
+    <div id="content">
+      <h2>Camion</h2>
+      <form>
+        <div class="fieldset flash" id="fsUploadProgress">
+          <span class="legend">Upload Queue</span>
+        </div>
+        <div id="divStatus">0 Files Uploaded</div>
+  			<div>
+  				<span id="spanButtonPlaceHolder"></span>
+  				<input id="btnCancel" type="button" value="Cancel All Uploads" onclick="swfu.cancelQueue();" disabled="disabled" style="margin-left: 2px; font-size: 8pt; height: 29px;" />
+  			</div>
+      </form>
+    </div>
   </body>
 </html>
