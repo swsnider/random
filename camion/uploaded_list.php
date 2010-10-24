ee127e331669ebaa1170a7931bf8f4191c2f9dc6
Silas Snider <swsnider@gmail.com>
2010-10-24 12:12:43 -0400

    typo

--- camion/uploaded_list.php
+++ camion/uploaded_list.php
@@ -8,7 +8,7 @@
   echo "<h3>Already uploaded by you</h3>\n";
   echo "<ul>\n";
   foreach ($dir_list as $value){
-    if ($value != '.' || $value != '..'){
+    if ($value !== '.' || $value !== '..'){
 ?>  <li><a href="download_file.php?type=user&amp;file=<?= $value ?>"><?= $value ?></a></li>
 <?php
     }
